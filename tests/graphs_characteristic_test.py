4b1ba4965ebd038ac7bb00e4a46a4693cdb05a10
Минаков Данил Дмитриевич <bsPython@yandex.ru>
2025-05-30 23:16:34 +0300

    test auto-checkers #2

--- tests/graphs_characteristic_test.py
+++ tests/graphs_characteristic_test.py
@@ -1,6 +1,13 @@
 import sys
 import os
 import numpy as np
+
+
+
+
+
+
+
 from graph_characteristic import (
     calculate_min_deg,
     calculate_max_deg,
@@ -9,12 +16,6 @@ from graph_characteristic import (
     calculate_number_triangle,
     Graph
 )
-
-
-
-
-
-
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", "src"))
 
 
